9fc3b75c9537d6a4c8f10f90de9bb89638246175
restartbutton <rogeriofbjr@hotmail.com>
2022-09-01 07:49:34 -0300

    Ajustada varredura para aceitar .com.br e letras maiusculas

--- src/buscapadraoweb/Main.java
+++ src/buscapadraoweb/Main.java
@@ -1,6 +1,7 @@
 //compile: javac -d build/classes src/buscaweb/*.java src/buscapadraoweb/*.java
 //run: java -cp build/classes buscapadraoweb/Main
-
+//compile-and-run: javac -d build/classes src/buscaweb/*.java src/buscapadraoweb/*.java && java -cp build/classes buscapadraoweb/Main
+//args: "http://www.mcce.org.br/newsletter" "https://www.univali.br/" "https://www.navship.com.br/"
 
 package buscapadraoweb;
 
@@ -45,141 +46,151 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try{
+        if(args.length > 0){
+            try{
 
-            System.out.println("Iniciando Conexao");
+                for(String arg : args){
+                    System.out.println("\nIniciando Conexao com " + arg);
 
-            //instancia e usa objeto que captura código-fonte de páginas Web
-            CapturaRecursosWeb crw = new CapturaRecursosWeb();
-            crw.getListaRecursos().add("http://www.mcce.org.br/newsletter");
-            ArrayList<String> listaCodigos = crw.carregarRecursos();
+                    //instancia e usa objeto que captura código-fonte de páginas Web
+                    CapturaRecursosWeb crw = new CapturaRecursosWeb();
+                    crw.getListaRecursos().add(arg);
+                    ArrayList<String> listaCodigos = crw.carregarRecursos();
 
-            System.out.println("Conexao Estabelecida");
+                    String codigoHTML = listaCodigos.get(0);
 
-            String codigoHTML = listaCodigos.get(0);
+                    if(codigoHTML == null){
+                        System.out.println("Falha na Conexao!");
+                    } else {
+                        System.out.println("HTML Obtido");
 
-            System.out.println("HTML Obtido");
+                        
 
-            //mapa do alfabeto
-            char[] alfabeto = "0123456789abcdefghijklmnopqrstuvwxyz@.-_".toCharArray();
+                        //mapa do alfabeto
+                        char[] alfabeto = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@.-_".toCharArray();
 
-            //mapa de estados
-            String[] estados = new String[6];
-            estados[0] = "q0";
-            estados[1] = "q1";
-            estados[2] = "q2";
-            estados[3] = "q3";
-            estados[4] = "q4";
-            estados[5] = "q5";
+                        //mapa de estados
+                        String[] estados = new String[6];
+                        estados[0] = "q0";
+                        estados[1] = "q1";
+                        estados[2] = "q2";
+                        estados[3] = "q3";
+                        estados[4] = "q4";
+                        estados[5] = "q5";
 
-            String estado_inicial = "q0";
+                        String estado_inicial = "q0";
 
-            //estados finais
-            String[] estados_finais = new String[2];
-            estados_finais[0] = "q4";
-            estados_finais[1] = "q5";
+                        //estados finais
+                        String[] estados_finais = new String[2];
+                        estados_finais[0] = "q4";
+                        estados_finais[1] = "q5";
 
-            //tabela de transição de AFD para reconhecimento números de dois dígitos
-            int[][] matriz = new int[estados.length][alfabeto.length];
+                        //tabela de transição de AFD para reconhecimento números de dois dígitos
+                        int[][] matriz = new int[estados.length][alfabeto.length];
 
-            //transições de q0
-            for(int i=0; i < alfabeto.length; i++) {
-                matriz[get_string_ref(estados, "q0")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '@')
-                    ? get_string_ref(estados, "q1")
-                    : get_string_ref(estados, "q0");
-            }
-
-            //transições de q1
-            for(int i=0; i < alfabeto.length; i++) {
-                matriz[get_string_ref(estados, "q1")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '@')
-                    ? -1
-                    : (alfabeto[i] == '.') 
-                    ? get_string_ref(estados, "q2")
-                    : get_string_ref(estados, "q1");
-            }
-
-            //transições de q2
-            for(int i=0; i < alfabeto.length; i++) {
-                matriz[get_string_ref(estados, "q2")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '@' || alfabeto[i] == '-' || alfabeto[i] == '_' || alfabeto[i] == '.')
-                    ? -1 
-                    : get_string_ref(estados, "q3");
-            }
-
-            //transições de q3
-            for(int i=0; i < alfabeto.length; i++) {
-                matriz[get_string_ref(estados, "q3")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '@' || alfabeto[i] == '-' || alfabeto[i] == '_' || alfabeto[i] == '.')
-                    ? -1 
-                    : get_string_ref(estados, "q4");
-            }
-
-            //transições de q4
-            for(int i=0; i < alfabeto.length; i++) {
-                matriz[get_string_ref(estados, "q4")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '@' || alfabeto[i] == '-' || alfabeto[i] == '_')
-                    ? -1 
-                    : (alfabeto[i] == '.')
-                    ? get_string_ref(estados, "q1")
-                    : get_string_ref(estados, "q5");
-            }
-
-            //transições de q5
-            for(int i=0; i < alfabeto.length; i++) {
-                matriz[get_string_ref(estados, "q5")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '.')
-                    ? get_string_ref(estados, "q1")
-                    : -1;
-            }
-
-            
-            int estado = get_string_ref(estados, estado_inicial);
-            int estado_anterior = -1;
-            ArrayList<String> palavras_reconhecidas = new ArrayList<String>();
-
-
-            String palavra = "";
-
-            System.out.println("Iniciando Varredura");
-
-            //varre o código-fonte de um código
-            for (int i=0; i<codigoHTML.length(); i++){
-
-                estado_anterior = estado;
-                estado = proximo_estado(alfabeto, matriz, estado, codigoHTML.charAt(i));
-                //se o não há transição
-                if (estado == -1){
-                    //pega estado inicial
-                    estado = get_string_ref(estados, estado_inicial);
-                    // se o estado anterior foi um estado final
-                    if (get_string_ref(estados_finais, estados[estado_anterior]) != -1){
-                        //se a palavra não é vazia adiciona palavra reconhecida
-                        if ( ! palavra.equals("")){
-                            palavras_reconhecidas.add(palavra);
+                        //transições de q0
+                        for(int i=0; i < alfabeto.length; i++) {
+                            matriz[get_string_ref(estados, "q0")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '@')
+                                ? get_string_ref(estados, "q1")
+                                : get_string_ref(estados, "q0");
                         }
-                        // se ao analisar este caracter não houve transição
-                        // teste-o novamente, considerando que o estado seja inicial
-                        i--;
+
+                        //transições de q1
+                        for(int i=0; i < alfabeto.length; i++) {
+                            matriz[get_string_ref(estados, "q1")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '@')
+                                ? -1
+                                : (alfabeto[i] == '.') 
+                                ? get_string_ref(estados, "q2")
+                                : get_string_ref(estados, "q1");
+                        }
+
+                        //transições de q2
+                        for(int i=0; i < alfabeto.length; i++) {
+                            matriz[get_string_ref(estados, "q2")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '@' || alfabeto[i] == '-' || alfabeto[i] == '_' || alfabeto[i] == '.')
+                                ? -1 
+                                : get_string_ref(estados, "q3");
+                        }
+
+                        //transições de q3
+                        for(int i=0; i < alfabeto.length; i++) {
+                            matriz[get_string_ref(estados, "q3")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '@' || alfabeto[i] == '-' || alfabeto[i] == '_' || alfabeto[i] == '.')
+                                ? -1 
+                                : get_string_ref(estados, "q4");
+                        }
+
+                        //transições de q4
+                        for(int i=0; i < alfabeto.length; i++) {
+                            matriz[get_string_ref(estados, "q4")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '@' || alfabeto[i] == '-' || alfabeto[i] == '_')
+                                ? -1 
+                                : (alfabeto[i] == '.')
+                                ? get_string_ref(estados, "q1")
+                                : get_string_ref(estados, "q5");
+                        }
+
+                        //transições de q5
+                        for(int i=0; i < alfabeto.length; i++) {
+                            matriz[get_string_ref(estados, "q5")][get_char_ref(alfabeto, alfabeto[i])] = (alfabeto[i] == '.')
+                                ? get_string_ref(estados, "q2")
+                                : -1;
+                        }
+
+                        
+                        int estado = get_string_ref(estados, estado_inicial);
+                        int estado_anterior = -1;
+                        ArrayList<String> palavras_reconhecidas = new ArrayList<String>();
+
+
+                        String palavra = "";
+
+                        System.out.println("Iniciando Varredura");
+
+                        //varre o código-fonte de um código
+                        for (int i=0; i<codigoHTML.length(); i++){
+
+                            estado_anterior = estado;
+                            estado = proximo_estado(alfabeto, matriz, estado, codigoHTML.charAt(i));
+                            //se o não há transição
+                            if (estado == -1){
+                                //pega estado inicial
+                                estado = get_string_ref(estados, estado_inicial);
+                                // se o estado anterior foi um estado final
+                                if (get_string_ref(estados_finais, estados[estado_anterior]) != -1){
+                                    //se a palavra não é vazia adiciona palavra reconhecida
+                                    if ( ! palavra.equals("")){
+                                        palavras_reconhecidas.add(palavra);
+                                    }
+                                    // se ao analisar este caracter não houve transição
+                                    // teste-o novamente, considerando que o estado seja inicial
+                                    i--;
+                                }
+                                //zera palavra
+                                palavra = "";
+                                
+                            } else {
+                                //se houver transição válida, adiciona caracter a palavra
+                                palavra += codigoHTML.charAt(i);
+                            }
+                        }
+
+                        System.out.println("Varredura Completa");
+
+                        if(palavras_reconhecidas.isEmpty()){
+                            System.out.println("Nenhuma Correspondencia Encontrada");
+                        } else{
+                            System.out.println("Listando Correspondencias Encontradas em " + arg + ":\n\n");
+
+                            //foreach no Java para exibir todas as palavras reconhecidas
+                            for (String p: palavras_reconhecidas){
+                                System.out.println (p);
+                            }
+                        }
                     }
-                    //zera palavra
-                    palavra = "";
-                    
-                } else {
-                    //se houver transição válida, adiciona caracter a palavra
-                    palavra += codigoHTML.charAt(i);
                 }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-
-            System.out.println("Varredura Completa");
-
-            if(palavras_reconhecidas.isEmpty()){
-                System.out.println("Nenhuma Correspondencia Encontrada");
-            } else{
-                System.out.println("Listando Correspondencias Encontradas:\n\n");
-
-                //foreach no Java para exibir todas as palavras reconhecidas
-                for (String p: palavras_reconhecidas){
-                    System.out.println (p);
-                }
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        } else {
+            System.out.println("Nenhum argumento passado!");
         }
 
 
